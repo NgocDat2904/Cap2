6abc97489bd6573884d27e8bbfb847601660f637
Ltrungnguyen <161904842+Ltrungnguyen@users.noreply.github.com>
2026-03-22 16:11:41 +0700

    up date lại

--- backend/app/modules/auth/auth_repository.py
+++ backend/app/modules/auth/auth_repository.py
@@ -3,9 +3,10 @@ from app.database.mongodb import db
 collection = db["users"]
 
 
-def create_user(user):
-    return collection.insert_one(user)
+def create_user(user_data):
+    result = db["users"].insert_one(user_data)
+    return str(result.inserted_id)
 
 
 def get_user_by_email(email: str):
-    return collection.find_one({"email": email})
+     return db["users"].find_one({"email": email})
