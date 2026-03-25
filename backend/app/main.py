4e29c6561db9afc806bd8a65f105638cf9488655
vietanhpyn <tranvietanh2@dtu.edu.vn>
2026-03-26 00:54:36 +0700

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.modules.auth.auth_controller import router as auth_router
+from app.modules.user.user_controller import router as user_router
 
+# 🔥 PHẢI tạo app trước
 app = FastAPI()
 
-# Chỉ định chính xác "nhà" của Frontend
+# CORS
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173"
@@ -18,7 +20,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# 🔥 include router sau khi có app
 app.include_router(auth_router)
+app.include_router(user_router)
 
 @app.get("/")
 def root():
